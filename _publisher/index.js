637b93c83b101d1d85f016d9c9e5beaac1695990
Greg <gjhamlin@gmail.com>
2022-01-24 07:42:14 -0500

    path

--- _publisher/index.js
+++ _publisher/index.js
@@ -1,12 +1,35 @@
-const PATH = require('path');
+const path = require('path');
 const dirTree = require('directory-tree');
 
-const tree = dirTree('../_examples', null, 
-(item, PATH, stats) => {
-    console.log("File: " + item.path);
-},
-    (item, PATH, stats) => {
-        console.log("Dir: " + item.path);
-    });
+const fs = require('fs');
 
-console.log(JSON.stringify(tree, null, 4));
+fs.readdirSync('..\\_examples').forEach(file => {
+  console.log(file);
+});
+
+console.log(path.resolve('..\\_examples'));
+
+// fs.lstatSync(path_string).isDirectory() 
+
+// const tree = dirTree('..\\_examples', null,
+//     (item, PATH, stats) => {
+//         console.log("File: " + item.path + " " + PATH);
+//     },
+//     (item, PATH, stats) => {
+//         console.log("Dir: " + item.path + " " + PATH);
+//     });
+
+// function processSubtree(tree) {
+//     for (let f of tree.children) {
+//         if (f.name.startsWith('build') ||
+//             f.name.startsWith('.git')) {
+//             continue;
+//         }
+//         console.log(f);
+//     }
+// }
+
+// function processProject() 
+// {
+
+// }
